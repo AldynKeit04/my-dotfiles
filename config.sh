76389d554e6264fae8d0b239efe867b1e89bb534
raziel raga <raziel@localhost.localdomain>
2024-08-31 16:00:55 -0400

    conf file

---
+++ config.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#display server
+sudo apt install -y xorg xbacklight xbindkeys xvkbd xinput xorg-dev
+
+#text editor
+sudo apt install -y neovim
+
+#python for coding
+sudo apt install -y python3-pip
+
+#microcode
+#sudo apt install -y intel microcode
+sudo apt install -y amd64-microcode
+
+#printing
+sudo apt install -y cups
+
+#audio server
+sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa
+
+#bluetooth
+sudo apt install -y bluez blueman
+
+#i3 of course
+sudo apt install -y i3
+
+#terminal
+sudo apt install -y rxvt-unicode
+
+#firefox
+sudo apt install -y firefox-esr
