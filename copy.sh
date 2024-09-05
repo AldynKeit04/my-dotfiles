7b98056a18ca8d3396d6e41683bb614d25f37cf7
raziel raga <raziel@localhost.localdomain>
2024-09-04 21:07:09 -0400

    added kitty config file and added copy info to copy.sh

--- copy.sh
+++ copy.sh
@@ -3,3 +3,5 @@
 cp ./conf/Wallpapers/wallpaper1.png ~/.config/i3
 cp ./conf/i3config/config ~/.config/i3
 cp ./conf/.bashrc ~/
+mkdir ~/.config/kitty
+cp ./conf/kittyconfig/kitty.conf ~/.config/kitty
