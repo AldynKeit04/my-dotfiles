563df1154bc0643b64936351e1991112da5f0c33
raziel raga <raziel@localhost.localdomain>
2024-09-05 00:11:38 -0400

    added picom conf and added to copy.sh

--- copy.sh
+++ copy.sh
@@ -3,6 +3,7 @@
 cp ./conf/Wallpapers/wallpaper1.png ~/.config/i3
 cp ./conf/i3config/config ~/.config/i3
 cp ./conf/.bashrc ~/
+cp ./conf/picom.conf ~/.config
 mkdir ~/.config/kitty
 cp ./conf/kittyconfig/kitty.conf ~/.config/kitty
 cp ./conf/layout_setup.sh ~/.config/i3
